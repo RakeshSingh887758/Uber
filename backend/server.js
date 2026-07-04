72486a831668e3c5e31387d29c013935675856cc
rakesh_singh <rakeshsinghdos2016@gmail.com>
2026-07-04 20:02:48 +0530

    initial stage

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const http=require("http");
+const app=require("./app.js");
+const PORT=process.env.PORT||4000;
+const server=http.createServer(app);
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on Port ${PORT}`);
+});
